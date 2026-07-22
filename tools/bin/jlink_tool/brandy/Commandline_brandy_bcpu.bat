ef218863e63ff77915f5e077e86c28cbc3b85ca3
farbeat <lixiangwu@greatteam.org.cn>
2026-07-22 21:01:00 +0800

    init code

---
+++ tools/bin/jlink_tool/brandy/Commandline_brandy_bcpu.bat
@@ -0,0 +1,3 @@
+TITLE BCPU Command Line
+"C:\Program Files (x86)\SEGGER\JLink\JLink.exe"   -device RISC-V -if SWD -speed 1000 -JLinkScriptFile ConnectCore2.JLinkScript  -autoconnect 1
+pause
---
+++ tools/bin/jlink_tool/brandy/Commandline_brandy_mcpu.bat
@@ -0,0 +1,3 @@
+TITLE MCPU Command Line
+"C:\Program Files (x86)\SEGGER\JLink\JLink.exe"   -device RISC-V -if SWD -speed 1000 -JLinkScriptFile ConnectCore1.JLinkScript  -autoconnect 1
+pause
